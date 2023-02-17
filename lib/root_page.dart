1c6910788fdc298d775b682e567b1b5ef81df902
unknown <avilasonson@gmail.com>
2023-02-18 07:56:29 +0800

    added rootpage

---
+++ lib/root_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RootPage extends StatefulWidget {
+  const RootPage({ Key? key }) : super(key: key);
+
+  @override
+  _RootPageState createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
