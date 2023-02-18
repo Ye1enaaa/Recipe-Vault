f525453565028ce222f810ee44927ada943d53c5
unknown <avilasonson@gmail.com>
2023-02-18 09:23:15 +0800

    added HomePage, Listings & ProfileSection

---
+++ lib/listings.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Listings extends StatefulWidget {
+  const Listings({ Key? key }) : super(key: key);
+
+  @override
+  _ListingsState createState() => _ListingsState();
+}
+
+class _ListingsState extends State<Listings> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Listings'),),
+    );
+  }
+}
--- lib/root_page.dart
+++ lib/root_page.dart
@@ -1,4 +1,9 @@
+import 'package:firstactivity/home_page.dart';
+import 'package:firstactivity/listings.dart';
+import 'package:firstactivity/profile_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:line_icons/line_icons.dart';
 
 class RootPage extends StatefulWidget {
   const RootPage({ Key? key }) : super(key: key);
@@ -8,10 +13,48 @@ class RootPage extends StatefulWidget {
 }
 
 class _RootPageState extends State<RootPage> {
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: IndexedStack(
+        index: selectedIndex,
+        children: const [
+          HomePage(),
+          Listings(),
+          ProfilePage()
+        ],
+      ),
+      bottomNavigationBar: GNav(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+        backgroundColor: Colors.blue,
+        gap: 8,
+        rippleColor:Colors.grey[300]!,
+        hoverColor:Colors.grey[100]!,
+        activeColor: Colors.black,
+        onTabChange: (index){
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        tabs: [
+          GButton(
+            icon: LineIcons.home,
+            text: 'Home',
+            borderRadius: BorderRadius.circular(20),
+          ),
+          GButton(
+            icon: LineIcons.bookmark,
+            text: 'Listings',
+            borderRadius: BorderRadius.circular(20),
+          ),
+          GButton(
+            icon: LineIcons.user,
+            text: 'Account',
+            borderRadius: BorderRadius.circular(20),
+          )    
+        ],
+      ),
     );
   }
 }
