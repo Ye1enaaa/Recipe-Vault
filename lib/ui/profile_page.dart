1dabb9a823447418e59e32f80b92d2c04455fe6a
unknown <avilasonson@gmail.com>
2023-02-21 21:50:44 +0800

    adding constant picture and text in profilepage

--- lib/ui/profile_page.dart
+++ lib/ui/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({ Key? key }) : super(key: key);
@@ -11,7 +12,43 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Profile'),),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 30),
+              Center(
+                child: Container( 
+                  height:200,
+                  width: 200,
+                decoration: BoxDecoration(
+                  border: Border.all(width: 2),
+                  boxShadow: [
+                    BoxShadow(
+                      spreadRadius: 2,
+                      blurRadius: 10,
+                      color: Colors.black.withOpacity(0.1)
+                    )
+                  ],
+                  shape: BoxShape.circle,
+                  image: const DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('assets/img/gojo.jpg')
+                  )
+                ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Text(
+                'Erickson Dela Cruz',
+                style: GoogleFonts.fredoka(
+                  fontSize: 24
+                ),
+              )
+            ],
+          ),    
+        )
+      ),
     );
   }
 }
